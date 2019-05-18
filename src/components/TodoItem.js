92f2af6d4da6bd730b7fc15cb41cc55911b6cf3c
SEC\user <dorothy_007@naver.com>
2019-05-18 18:49:08 +0900

    컴포넌트 최적화
    TodoItemList, TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.value.checked !== nextProps.value.checked;
+  }
+
   render() {
     const {selectKey,onRemove,onToggle} = this.props;
     const {text,checked,color} = this.props.value;
-
+    console.log(selectKey);
     return(
-      <div className="todoItem" style={{color}} onClick={() => onToggle(selectKey,checked)}>
+        <div className="todoItem" style={{color}} onClick={() => onToggle(selectKey,checked)}>
         <div className="remove" onClick={(e) => {
             e.stopPropagation(); // onToggle 이 실행되지 않도록 함
             onRemove(selectKey)}
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+
   render() {
     const {todos,onRemove,onToggle} = this.props;
-
+    console.log("TodoItemList");
     const todoItemList = todos.map(
       (value, index) => (
         <TodoItem
